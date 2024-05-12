6455c038597aed0eeee170ce226baaf250eca3b6
Mohammed Alfilfil <moh151alfilfil@gmail.com>
2024-05-12 21:34:20 +0300

    filfil-test1

---
+++ src/main/java/com/example/swe206project/Test.java
@@ -0,0 +1,10 @@
+package com.example.swe206project;
+
+public class Test {
+    public static void main(String[] args) {
+
+        User user1 = new User("Ali", 1, "test.gmail.com", "male");
+
+        System.out.println(user1);
+    }
+}
